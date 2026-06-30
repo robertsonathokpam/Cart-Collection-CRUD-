38ae2e1a659bfd28991bf68ebb7dee54a42671ca
robertsonathokpam <153417457+robertsonathokpam@users.noreply.github.com>
2026-06-30 22:53:28 +0530

    commit

---
+++ todo/server/index.js
@@ -0,0 +1,85 @@
+const express= require('express');
+const app= express();
+const cors=require('cors');
+const pool=require('./db');
+app.use(cors());
+app.use(express.json());
+
+
+app.post("/todos",async(req,res)=>{
+    try {
+       // console.log(req.body); // this means that we are able to get the data from the frontend
+       const { description } = req.body;
+
+       //in simple language, the purpose of this newtodo is we are inserting the data into the database and returning the newly created todo item as a response in the form of JSON in the res which we can use in the frontend.    
+      const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES($1) RETURNING *",
+      [description]
+    );
+
+    res.json(newTodo.rows[0]); //purpose of this is to return the newly created todo item as a response in the form of JSON in the res which we can use in the frontend.
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+//get all todos
+
+app.get("/todos", async (req, res) => {
+  try {
+    const allTodos = await pool.query("SELECT * FROM todo");
+    res.json(allTodos.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+//get a todo
+
+app.get("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [
+      id
+    ]);
+
+    res.json(todo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+//update a todo
+
+app.put("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+    const updateTodo = await pool.query(
+      "UPDATE todo SET description = $1 WHERE todo_id = $2",
+      [description, id]
+    );
+
+    res.json("Todo was updated!");
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+//delete a todo
+
+app.delete("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [
+      id
+    ]);
+    res.json("Todo was deleted!");
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
